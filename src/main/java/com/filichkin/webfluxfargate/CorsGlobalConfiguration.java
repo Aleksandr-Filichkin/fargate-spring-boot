af3517870fd6439da0f8dd7b769d4d92e70c2666
Aleksandr_Filichkin <Aleksandr.Filichkin@refinitiv.com>
2022-09-11 02:05:13 +0200

    fix CORS

---
+++ src/main/java/com/filichkin/webfluxfargate/CorsGlobalConfiguration.java
@@ -0,0 +1,19 @@
+//package com.filichkin.webfluxfargate;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.reactive.config.CorsRegistry;
+//import org.springframework.web.reactive.config.EnableWebFlux;
+//import org.springframework.web.reactive.config.WebFluxConfigurer;
+//
+//import java.util.List;
+//
+//@Configuration
+//@EnableWebFlux
+//public class CorsGlobalConfiguration implements WebFluxConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry corsRegistry) {
+//        corsRegistry.addMapping("/**")
+//                .allowedMethods("*");
+//    }
+//}
